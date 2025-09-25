00c70ee9b838957abd20b5f323f6479088c17eed
New-B <wang0108153077@163.com>
2025-09-24 22:04:16 -0400

    diff perc. of remote create

--- tests/leopar_mb_create_join.c
+++ tests/leopar_mb_create_join.c
@@ -28,17 +28,21 @@ static void* noop(void *arg) { (void)arg; return NULL; }
 
 int main(int argc, char **argv)
 {
-    if (argc < 7) {
-        fprintf(stderr, "usage: %s <cluster.ini> <rank> <log_path> <peer_rank> <iters> <mode>\n", argv[0]);
-        fprintf(stderr, "  mode: remote|local|auto\n");
+    // if (argc < 7) {
+    //     fprintf(stderr, "usage: %s <cluster.ini> <rank> <log_path> <peer_rank> <iters> <mode>\n", argv[0]);
+    //     fprintf(stderr, "  mode: remote|local|auto\n");
+    //     return 1;
+    // }
+    if (argc < 3 || argc > 4) {
+        fprintf(stderr, "Usage: %s <config_path> <rank> [log_path]\n", argv[0]);
         return 1;
     }
     const char *cfg = argv[1];
     int my_rank     = atoi(argv[2]);
     const char *log_path = argv[3];
-    int peer_rank   = atoi(argv[4]);
-    int iters       = atoi(argv[5]);
-    const char *mode= argv[6];
+    //int peer_rank   = atoi(argv[4]);
+    int iters       = 10000;
+    // const char *mode= argv[6];
 
     if (leopar_init(cfg, my_rank, log_path) != 0) {
         fprintf(stderr, "leopar_init failed\n");
@@ -46,38 +50,78 @@ int main(int argc, char **argv)
     }
     if(my_rank == 0){
         int target = 0;
-        if (!strcmp(mode,"remote")) target = peer_rank;
-        else if (!strcmp(mode,"local")) target = my_rank;
-        else if (!strcmp(mode,"auto")) target = -1;
-        else { fprintf(stderr, "unknown mode\n"); return 1; }
-
+        // //if (!strcmp(mode,"remote")) target = peer_rank;
+        // else if (!strcmp(mode,"local")) target = my_rank;
+        // else if (!strcmp(mode,"auto")) target = -1;
+        // else { fprintf(stderr, "unknown mode\n"); return 1; }
+        double *tc = (double*)malloc(sizeof(double)*iters);
+        double *tj = (double*)malloc(sizeof(double)*iters);
+        double *tt = (double*)malloc(sizeof(double)*iters);
+        
         /* warmup */
         for (int i=0;i<100;i++) {
             leo_thread_t t; leo_thread_create(&t, NULL, noop, NULL, target);
             leo_thread_join(t, NULL);
         }
 
-        double *tc = (double*)malloc(sizeof(double)*iters);
-        double *tj = (double*)malloc(sizeof(double)*iters);
-        double *tt = (double*)malloc(sizeof(double)*iters);
 
-        for (int i=0;i<iters;i++) {
-            double t0 = now_us();
-            leo_thread_t t; leo_thread_create(&t, NULL, noop, NULL, target);
-            double t1 = now_us();
-            leo_thread_join(t, NULL);
-            double t2 = now_us();
-            tc[i] = t1 - t0;
-            tj[i] = t2 - t1;
-            tt[i] = t2 - t0;
-        }
 
-        if (my_rank == 0) {
-            printf("=== create/join latency (%s) ===\n", mode);
+        // for (int i=0;i<iters;i++) {
+        //     double t0 = now_us();
+        //     leo_thread_t t; leo_thread_create(&t, NULL, noop, NULL, target);
+        //     double t1 = now_us();
+        //     leo_thread_join(t, NULL);
+        //     double t2 = now_us();
+        //     tc[i] = t1 - t0;
+        //     tj[i] = t2 - t1;
+        //     tt[i] = t2 - t0;
+        // }
+        
+        // printf("=== create/join latency (local) ===\n");
+        // print_stats("create", tc, iters);
+        // print_stats("join",   tj, iters);
+        // print_stats("total",  tt, iters);
+
+        // target = 1;
+
+        // for (int i=0;i<iters;i++) {
+        //     double t0 = now_us();
+        //     leo_thread_t t; leo_thread_create(&t, NULL, noop, NULL, target);
+        //     double t1 = now_us();
+        //     leo_thread_join(t, NULL);
+        //     double t2 = now_us();
+        //     tc[i] = t1 - t0;
+        //     tj[i] = t2 - t1;
+        //     tt[i] = t2 - t0;
+        // }
+
+        // printf("=== create/join latency (remote) ===\n");
+        // print_stats("create", tc, iters);
+        // print_stats("join",   tj, iters);
+        // print_stats("total",  tt, iters);
+
+        for (int p=0; p < 6; p++){
+            for (int i=0;i<iters;i++) {
+                if(i <= p*0.2*iters){
+                    target = 0;
+                } else {
+                    target = 1;
+                }
+                double t0 = now_us();
+                leo_thread_t t; leo_thread_create(&t, NULL, noop, NULL, target);
+                double t1 = now_us();
+                leo_thread_join(t, NULL);
+                double t2 = now_us();
+                tc[i] = t1 - t0;
+                tj[i] = t2 - t1;
+                tt[i] = t2 - t0;
+            }
+            printf("=== create/join latency (remote percentage: %f) ===\n", p*0.2);
             print_stats("create", tc, iters);
             print_stats("join",   tj, iters);
             print_stats("total",  tt, iters);
         }
+
 
         free(tc); free(tj); free(tt);
         /* synchronize all ranks before finalize */
